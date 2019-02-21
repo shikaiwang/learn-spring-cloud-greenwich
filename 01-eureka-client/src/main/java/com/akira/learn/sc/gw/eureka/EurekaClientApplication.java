e0dcf06229c3c0f3656adc6dcd21b70475d2205c
AkiraLiu39 <liuchang8939@126.com>
2019-02-21 14:21:24 +0800

    hystrix 断路器监控

--- 01-eureka-client/src/main/java/com/akira/learn/sc/gw/eureka/EurekaClientApplication.java
+++ 01-eureka-client/src/main/java/com/akira/learn/sc/gw/eureka/EurekaClientApplication.java
@@ -1,9 +1,14 @@
 package com.akira.learn.sc.gw.eureka;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+//允许发现服务
+@EnableDiscoveryClient
+//使用Hystrix
+@EnableHystrix
+//开启断路器监控
+//仪表页面/hystrix
+// /actuator/hystrix.stream
+//需要先有消费者调用 否则/hystrix 一直loading。actuator/hystrix.stream一直ping不到数据
+@EnableHystrixDashboard
+//使用断路器
+@EnableCircuitBreaker
 public class EurekaClientApplication {
 
     @Value("${server.port}")
@@ -22,8 +38,13 @@ public class EurekaClientApplication {
 
 
     @GetMapping("/hi")
+    @HystrixCommand(fallbackMethod = "honeFallback")
     public String home(@RequestParam("name") String name) {
         return "hi " + name + " , this is from port:" + port;
+    }
+
+    public String honeFallback(String name) {
+        return "emmmm" + name + " i'm error! this is fallback method";
     }
 
 }
