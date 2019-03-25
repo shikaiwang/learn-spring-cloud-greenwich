fe06a2f9d4166c261bb3dc3bfd7b7c4ca519b024
AkiraLiu39 <liuchang8939@126.com>
2019-03-25 14:36:25 +0800

    oauth

---
+++ 09-monitor/09-01-admin-client/src/main/java/com/akira/learn/monitor/client/MonitorClientApplication.java
@@ -0,0 +1,30 @@
+package com.akira.learn.monitor.client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class MonitorClientApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorClientApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf()
+                    .ignoringAntMatchers("/actuator/**");
+        }
+    }
+}
